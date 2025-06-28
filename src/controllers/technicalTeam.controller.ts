83d12330df7e90b7c11401f6e7f2208a2ddd1334
SebasYnojosa <emilio240104@gmail.com>
2025-06-28 13:44:43 -0400

    Se agrego la busqueda por especialidad del equipo tecnico

--- src/controllers/technicalTeam.controller.ts
+++ src/controllers/technicalTeam.controller.ts
@@ -1,9 +1,31 @@
 import { createCrud } from './crudFactory';
 import { TechnicalTeam } from '../db/schema/schema';
 import { TechnicalTeamSchema } from '../db/schema/validationSchema';
+import { Request, Response } from 'express';
+import { eq } from 'drizzle-orm';
+import { db } from '../db';
 
-export const technicalTeamController = createCrud({
+const baseTechnicalTeamController = createCrud({
 	table: TechnicalTeam,
 	validationSchema: TechnicalTeamSchema,
 	objectName: 'TechnicalTeam'
 });
+
+export const technicalTeamController = {
+	...baseTechnicalTeamController,
+
+	async getBySpeciality(req: Request, res: Response) {
+		try {
+			const {speciality} = req.params;
+			const result = await db
+				.select()
+				.from(TechnicalTeam)
+				.where(eq(TechnicalTeam.speciality, speciality))
+			return res.status(200).json(result);
+		} catch (error) {
+			return res.status(500).json({ error: "internal server error", details: error.message })
+		}
+	}
+}
+
+
